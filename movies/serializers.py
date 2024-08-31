dd845a768cf4100edf32a7063b36c692301fa7ee
xMarkhus <marcosmiyamotoshigeru@gmail.com>
2024-08-31 08:15:58 -0300

    Add rate field in movies | stars_avg

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,9 +1,19 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        
+        return None
