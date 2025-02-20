0599c6a270faa627ab29816e8a5c155a51b77981
xMarkhus <marcosmiyamotoshigeru@gmail.com>
2025-02-20 20:44:40 -0300

    Config for docs

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
+def redirect_to_docs(request):
+    return redirect('/swagger/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', redirect_to_docs),
+    # URLS SWAGGER/REDOC
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     path('api/v1/', include('authentication.urls')),
     path('api/v1/', include('genres.urls')),
